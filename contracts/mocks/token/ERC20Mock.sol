b368d0757e9e578c4e61e449dff01a9f93afc7fb
failfmi <oscurocalma@gmail.com>
2021-11-03 10:01:11 +0200

    test(mocks): remove unused function
    Signed-off-by: failfmi <oscurocalma@gmail.com>

--- contracts/mocks/token/ERC20Mock.sol
+++ contracts/mocks/token/ERC20Mock.sol
@@ -4,15 +4,9 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Mock is ERC20 {
-    uint8 private constant _decimals = 18;
-
     constructor() ERC20("ERC20Mock", "MOCK-ERC20") {}
 
     function mint(address _account, uint256 _amount) public {
         super._mint(_account, _amount);
-    }
-
-    function decimals() public view virtual override returns (uint8) {
-        return _decimals;
     }
 }
