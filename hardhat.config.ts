92428b8a502dac2862f761ce47e3d0de57c95062
Daniel Ivanov <daniel.k.ivanov95@gmail.com>
2022-02-05 10:08:20 +0200

    feat(scripts): Add deployment script for metaverse consumable adapter facet
    Signed-off-by: Daniel Ivanov <daniel.k.ivanov95@gmail.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,23 +13,32 @@ task('deploy', 'Deploys the LandWorks Diamond with all of its facets')
         await deployLandWorks();
     });
 
-task('removeFacets', 'Removes MarketplaceFacet & DecentralandFacet from Diamond Router contract')
+task('removeMarketplaceDCLFacets', 'Removes MarketplaceFacet & DecentralandFacet from LandWorks Diamond' +
+    ' contract')
     .addParam('diamond', 'The address of the Diamond Contract')
     .addParam('marketplaceFacet', 'The address of MarketplaceFacet')
     .addParam('decentralandFacet', 'The address of DecentralandFacet')
     .setAction(async (taskArgs) => {
         console.log(taskArgs);
-        const removeFacets = require('./scripts/remove-facets');
-        await removeFacets(taskArgs.diamond, taskArgs.marketplaceFacet, taskArgs.decentralandFacet);
+        const removeMarketplaceDCLFacets = require('./scripts/remove-marketplace-dcl-facets');
+        await removeMarketplaceDCLFacets(taskArgs.diamond, taskArgs.marketplaceFacet, taskArgs.decentralandFacet);
     });
 
-task('addFacets', 'Deploys MarketplaceFacet & DecentralandFacet and Adds them to Diamond Router contract')
+task('addMarketplaceDCLFacets', 'Deploys MarketplaceFacet & DecentralandFacet and Adds them to LandWorks' +
+    'Diamond contract')
     .addParam('diamond', 'The address of the Diamond Contract')
     .setAction(async (taskArgs) => {
         console.log(taskArgs);
-        const addFacets = require('./scripts/add-facets');
-        await addFacets(taskArgs.diamond);
+        const addMarketplaceDCLFacets = require('./scripts/add-marketplace-dcl-facets');
+        await addMarketplaceDCLFacets(taskArgs.diamond);
     });
+
+task('addConsumableAdapterFacet', 'Deploys MetaverseConsumableAdapterFacet and adds it to Diamond')
+    .addParam('diamond', 'The address of the Diamond Contract')
+    .setAction(async (taskArgs) => {
+        const addConsumableAdapterFacet = require('./scripts/add-consumable-adapter-facet');
+        await addConsumableAdapterFacet(taskArgs.diamond);
+    })
 
 module.exports = {
     solidity: {
